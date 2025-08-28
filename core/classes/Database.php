b26534a11541d6ecfcd6e871d22e44926d76b9d8
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-08-28 20:31:00 -0300

    criação da ligação com a base de daos e metodo select

--- core/classes/Database.php
+++ core/classes/Database.php
@@ -2,15 +2,56 @@
 
 namespace core\classes;
 
+use PDO;
+use PDOException;
+
 class Database{
 
-    
+    private $pdo;
+
+    private function ligar(){
+        // ligar a base de dados
+       $this->pdo = new PDO(
+            "mysql:host=".SERVER.
+            ";dbname=".DATABASE,
+            USER,
+            PASS,
+            array(PDO::ATTR_PERSISTENT => true)
+        );
+        // debug
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    private function desligar(){
+        // desligar a base de dados
+        $this->pdo = null;
+    }
+
+    // metodo de pesquisa sql
+    public function select($sql, $param = null){
+        $this->ligar();
+
+        $res = null;
+
+        // comunicar
+        try {
+            if(!empty($param)){
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->execute($param);
+                $res = $stmt->fetchAll(PDO::FETCH_CLASS);
+            }else{
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->execute();
+                $res = $stmt->fetchAll(PDO::FETCH_CLASS);
+            }
+        } catch (PDOException $e) {
+            return false;
+        }
+
+        // desconectar da base de dados
+        $this->desligar();
+        // retorna o objeto
+        return $res;
+    }
+
 }
-
-
-
-// MYSQL
-define("SERVER",        "localhost");
-define("DATABASE",      "web_store");
-define("PASS",          "");
-define("USER",          "root");
